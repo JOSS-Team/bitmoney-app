e8634aa2a9b17570067a27d0917c331eda1d1363
Awrysfab <anwarr621@gmail.com>
2020-06-11 12:02:25 +0700

    add input outcome

--- app/src/main/java/com/awrcorp/bitmoney_app/ui/main/home/HomeViewModel.kt
+++ app/src/main/java/com/awrcorp/bitmoney_app/ui/main/home/HomeViewModel.kt
@@ -4,7 +4,11 @@ import androidx.lifecycle.ViewModel
 import com.awrcorp.bitmoney_app.repository.AppRepository
 
 class HomeViewModel(private val appRepository: AppRepository): ViewModel() {
-    private val userId = appRepository.id
+    val userId = appRepository.id
     val histories = appRepository.getHistories(userId)
     val user = appRepository.getUser(userId)
+    fun updateUser(userId: Int, nama: String, email: String, password: String, balance: Int) =
+            appRepository.updateUser(userId, nama, email, password, balance)
+    fun addOutcome(name: String, amount: Int, category: String, date: String, isPlan: Boolean, user: Int) =
+            appRepository.addOutcome(name, amount, category, date, isPlan, user)
 }
